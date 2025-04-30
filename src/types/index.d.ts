426d6dab733671e0bc66710c055923535b8a2d4b
douglasmatosdev <douglasmatosdev@gmail.com>
2025-04-30 08:51:57 -0300

    mensagem

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,6 +1,10 @@
+declare type DogStatus = 'available' | 'pending' | 'adopted';
+
 declare type Dog = {
   id: number;
   name: string;
   mainImage: string;
   moreImages: string[];
+  status: DogStatus;
+  gender: 'male' | 'female'
 };
